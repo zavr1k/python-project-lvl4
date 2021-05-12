f1c56028c56ff18aab07da62df068de5de279a42
zavr1k <zavr35rus@gmail.com>
2021-05-12 14:39:49 +0300

    added allowed host

--- tasks_manager/settings.py
+++ tasks_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['sleepy-savannah-96954.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
