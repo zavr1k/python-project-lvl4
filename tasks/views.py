146f361aaf7d69d5d53bfc733022b31d01851539
Anton Semenov <s.anton@live.ru>
2021-06-26 11:30:10 +0300

    clear template

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from task_manager.settings import STATIC_ROOT
 
 
 def home(request):
-    return render(request, 'tasks/main.html', {'sr': STATIC_ROOT})
+    return render(request, 'tasks/main.html')
