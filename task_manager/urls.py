226aa2e01533c3c45014af14645baa7de648f782
Anton Semenov <s.anton@live.ru>
2021-06-26 23:31:32 +0300

    added internationalization

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('tasks.urls')),
     path('admin/', admin.site.urls),
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 
 def home(request):
-    return render(request, 'tasks/main.html')
+    content = {
+        'title': _('Task manager')
+    }
+    return render(request, 'tasks/main.html', content)
