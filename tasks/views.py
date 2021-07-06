0c740aa08c052e50f98c9c9a3ccab0c6736e5e44
Anton Semenov <s.anton@live.ru>
2021-07-06 22:44:09 +0300

    create register users

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from django.views import View
+from django.views.generic import CreateView
+
+from .forms import RegisterUserForm
 
 
 class Home(View):
@@ -10,3 +14,14 @@ class Home(View):
             'title': _('Task manager')
         }
         return render(request, 'tasks/main.html', content)
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'tasks/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super(RegisterUser, self).get_context_data(**kwargs)
+        context['title'] = _('Register')
+        return context
