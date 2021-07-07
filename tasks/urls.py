492ee7c149797eab15360f05d360388635cd2847
Anton Semenov <s.anton@live.ru>
2021-07-07 09:23:15 +0300

    create authentication

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home, RegisterUser
-
+from .views import Home, RegisterUser, LoginUser, logout_user
 
 urlpatterns = [
     path('',  Home.as_view(), name='home'),
-    path('register/', RegisterUser.as_view(), name='register')
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from django.views import View
 from django.views.generic import CreateView
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterUserForm
 
 
@@ -25,3 +27,21 @@ class RegisterUser(CreateView):
         context = super(RegisterUser, self).get_context_data(**kwargs)
         context['title'] = _('Register')
         return context
+
+
+class LoginUser(LoginView):
+    form = AuthenticationForm
+    template_name = 'tasks/login.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(LoginUser, self).get_context_data(**kwargs)
+        context['title'] = _('Authentication')
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
