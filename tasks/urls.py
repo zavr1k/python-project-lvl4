dec681ce9868dcd1e532b40af6d628899c1432a0
Anton Semenov <s.anton@live.ru>
2021-07-09 21:44:11 +0300

    created update and delete user

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import Home, RegisterUser, LoginUser, logout_user
+from .views import Home, RegisterUser, LoginUser, logout_user, UserList, UpdateUser, DeleteUser
 
 urlpatterns = [
     path('',  Home.as_view(), name='home'),
-    path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
+    path('users/', UserList.as_view(), name='user_list'),
+    path('users/create/', RegisterUser.as_view(), name='register'),
+    path('users/<int:pk>/update/', UpdateUser.as_view(), name='change_user'),
+    path('users/<int:pk>/delete/', DeleteUser.as_view(), name='delete_user'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,61 @@
 from django.contrib.auth import logout, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django.views import View
-from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib import messages
+
 from .forms import RegisterUserForm
+
+
+class UserList(ListView):
+    model = User
+    template_name = 'tasks/user_list.html'
+    context_object_name = 'users'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Users')
+        return context
+
+
+class UpdateUser(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'tasks/update_user.html'
+    form_class = RegisterUserForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('User change')
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        if kwargs['pk'] != self.request.user.pk:
+            messages.error(self.request, _("You are not allowed change other users"))
+            return redirect('user_list')
+        return super(UpdateUser, self).dispatch(request, *args, **kwargs)
+
+
+class DeleteUser(LoginRequiredMixin, DeleteView):
+    model = User
+    template_name = 'tasks/delete_user.html'
+    success_url = reverse_lazy('user_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Delete user')
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        if kwargs['pk'] != self.request.user.pk:
+            messages.error(self.request, _("You are not allowed change other users"))
+            return redirect('user_list')
+        return super(DeleteUser, self).dispatch(request, *args, **kwargs)
 
 
 class Home(View):
