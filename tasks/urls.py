c3a7e8457524eebd3a1a91e08ca8ede7927be195
Anton Semenov <s.anton@live.ru>
2021-08-12 09:25:22 +0300

    error for check rollbar

--- tasks/urls.py
+++ tasks/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('statuses/<int:pk>/delete', DeleteStatus.as_view(),
          name='delete_status'),
     path('tasks/', TaskList.as_view(), name='task_list'),
-    path('tasks/create/', CreateTask.as_view(), name='create_task'),
+    path('tasks/create/', CreateTask.as_view(), name='creat_task'),
     path('tasks/<int:pk>/update/', UpdateTask.as_view(), name='update_task'),
     path('tasks/<int:pk>/delete/', DeleteTask.as_view(), name='delete_task'),
     path('labels/', LabelList.as_view(), name='label_list'),
